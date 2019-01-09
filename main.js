c066a2e7fa225ce7fb1e862af58000f5097e21fb
aarmlovich <aarmlovi@gmail.com>
2019-01-09 17:45:16 -0500

    add popup and fox poking and header

---
+++ main.js
@@ -0,0 +1,17 @@
+alert('Warning, this is a popup alert!!!');
+
+var myHeading - document.querySelector('h1');
+
+heading.textContent = 'Hello world!';
+
+
+ // Define a Function
+ function sayOuch() {
+ 	alert('Ouch! Stop poking me!');
+ }
+ 
+ // Use a CSS selector to identify an element
+ var foxImage = document.querySelector('img');
+ 
+ // Assign the function to the onclick event on that element
+ foxImage.onclick = sayOuch;
